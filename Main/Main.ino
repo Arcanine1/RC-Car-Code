39fe95d7313c6662166d864f3e51260864ece4f7
Ayush548 <ayushgarg0728@gmail.com>
2023-08-04 17:51:30 -0400

    Added Bluetooth

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,3 +1,6 @@
+#include <SoftwareSerial.h>;
+SoftwareSerial BT(3, 4); 
+
 # define leftOne 5
 # define leftTwo 6
 # define rightOne 7
@@ -12,12 +15,39 @@ void setup() {
   pinMode(rightOne, OUTPUT);
   pinMode(rightTwo, OUTPUT);
 
+  // set the data rate for the SoftwareSerial port
+  BT.begin(9600);
+  // Send test message to other device
+  BT.println("Hello from Arduino");
 
 }
+
+char a; // stores incoming character from other device
 
 void loop() {
-  test();
+
+    if (BT.available())
+  // if text arrived in from BT serial...
+  {
+    a=(BT.read());
+    if (a=='1')
+    {
+      BT.println("LED on");
+    }
+    if (a=='2')
+    {
+      BT.println("LED off");
+    }
+    if (a=='?')
+    {
+      BT.println("Send '1' to turn LED on");
+      BT.println("Send '2' to turn LED on");
+    }   
+    // you can add more "if" statements with other characters to add more commands
+  }
+
 }
+
 
 
 void forward(){
