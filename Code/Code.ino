af7057cd1ff45bdb55676b1c0af7622b0b8c802e
Ayush548 <ayushgarg0728@gmail.com>
2023-08-03 23:18:01 -0400

    Made Test Code

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,7 +1,7 @@
-# define leftOne 4
-# define leftTwo 5
-# define rightOne 6
-# define rightTwo 7
+# define leftOne 5
+# define leftTwo 6
+# define rightOne 7
+# define rightTwo 8
 
 
 void setup() {
@@ -13,14 +13,37 @@ void setup() {
   pinMode(rightTwo, OUTPUT);
 
 
-
 }
 
 void loop() {
+  
   digitalWrite(leftOne,HIGH);
+  digitalWrite(leftTwo,LOW);
+  digitalWrite(rightOne,LOW);
+  digitalWrite(rightTwo,LOW);
+  delay(2000);
+
+  digitalWrite(leftOne,LOW);
+  digitalWrite(leftTwo,HIGH);
+  digitalWrite(rightOne,LOW);
+  digitalWrite(rightTwo,LOW);
+  delay(2000);
+
+  digitalWrite(leftOne,LOW);
   digitalWrite(leftTwo,LOW);
   digitalWrite(rightOne,HIGH);
   digitalWrite(rightTwo,LOW);
+  delay(2000);
 
+    digitalWrite(leftOne,LOW);
+  digitalWrite(leftTwo,LOW);
+  digitalWrite(rightOne,LOW);
+  digitalWrite(rightTwo,HIGH);
+  delay(2000);
 
+      digitalWrite(leftOne,HIGH);
+  digitalWrite(leftTwo,LOW);
+  digitalWrite(rightOne,LOW);
+  digitalWrite(rightTwo,HIGH);
+  delay(2000);
 }
